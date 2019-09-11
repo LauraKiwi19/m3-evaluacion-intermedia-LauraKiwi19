8411e23605597f80ec7bafa8d2c4527ab149710a
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-09-11 17:04:39 +0200

    add npm run build

---
+++ docs/precache-manifest.318b0d8fbfd87f22d604a17acb354ecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82ea93f34560c05173f1e52fc8e78a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04f3b443e607f8e59d70",
+    "url": "/static/css/main.46d99642.chunk.css"
+  },
+  {
+    "revision": "b1521f3f121bed9a918d",
+    "url": "/static/js/2.843fc558.chunk.js"
+  },
+  {
+    "revision": "04f3b443e607f8e59d70",
+    "url": "/static/js/main.7a6b3cea.chunk.js"
+  },
+  {
+    "revision": "22afbc056af1998ba8a3",
+    "url": "/static/js/runtime~main.6f933e2c.js"
+  }
+]);
