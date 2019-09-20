96dcbfe4e893010e1a8ed1ac53dc31a75488927a
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-09-20 19:40:36 +0200

    add docs

---
+++ docs/precache-manifest.8198a15ed19908122410034cc15510ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300578bbe6fdfb9b3a3dec8b982a23b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf5e7e2474a4e4bff53f",
+    "url": "./static/css/main.46d99642.chunk.css"
+  },
+  {
+    "revision": "b1521f3f121bed9a918d",
+    "url": "./static/js/2.843fc558.chunk.js"
+  },
+  {
+    "revision": "cf5e7e2474a4e4bff53f",
+    "url": "./static/js/main.1b32c4c6.chunk.js"
+  },
+  {
+    "revision": "847d5e4fed3c54fe368a",
+    "url": "./static/js/runtime~main.3491b175.js"
+  }
+]);
